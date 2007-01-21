8c8bae406c9edaf1c84b3c93cb0f5c8996890627
jnkim <jnkim@4bc5593c-822e-11de-b41d-d982dfe7be50>
2007-01-21 22:03:02 +0000

    git-svn-id: http://qmcpack.googlecode.com/svn@1635 4bc5593c-822e-11de-b41d-d982dfe7be50

--- branches/omp200701/src/SandBox/cubicspline1.cpp
+++ branches/omp200701/src/SandBox/cubicspline1.cpp
@@ -15,21 +15,43 @@
 #include "Message/CommOperators.h"
 #include "Message/CommunicateGroup.h"
 
+/** Implements a screened Function \f$u[r]=(1-z(r/rcut))/(1+B*z(r/rcut)\f$
+ * 
+ * Short-range functor introduced by Wagner and Mitas, cond-mat/0610088
+ */
 struct TestFunc1 {
-  double k0;
-  double minusksq;
-  TestFunc1(int nk0=1, double L=1.0) {
-    const double twopi = 8.0*std::atan(1.0);
-    k0=twopi*static_cast<double>(nk0)/L;
-    minusksq = -k0*k0;
+  typedef double real_type;
+  ///input B
+  real_type B0;
+  ///input Rcut
+  real_type Rcut;
+  ///1/Rcut
+  real_type OneOverRc;
+  ///id
+  string ID;
+  ///constructor
+  explicit TestFunc1(real_type b, real_type rc=7.5) {
+    reset(b,rc);
   }
-  //inline double f(double x) { return std::cos(k0*x); }
-  //inline double df(double x) { return -k0*std::sin(k0*x); }
-  //inline double d2f(double x) { return minusksq*std::cos(k0*x); }
-  inline double f(double x) { return std::sin(k0*x); }
-  inline double df(double x) { return k0*std::cos(k0*x); }
-  inline double d2f(double x) { return minusksq*std::sin(k0*x); }
+  inline void reset() { OneOverRc=1.0/Rcut; }
+  void reset(real_type b, real_type rc) { B0=b; Rcut=rc; reset(); }
+
+  inline real_type f(real_type r) {
+    real_type x=r*OneOverRc;
+    real_type z=x*x*(6.0-8*x+3.0*x*x);
+    return (1-z)/(1+B0*z);
+  }
+  inline real_type df(real_type r) {
+    real_type x=r*OneOverRc;
+    real_type z=x*x*(6.0-8*x+3.0*x*x);
+    return -(1+B0)/(1+B0*z)*(1+B0*z)*OneOverRc*12*x*(1-2.0*x+x*x);
+  }
+  inline real_type d2f(real_type r)
+  {
+    return 1.0;
+  }
 };
+
 
 struct ComboFunc1 {
 
@@ -47,13 +69,12 @@ struct ComboFunc1 {
   void push_back(double c, TestFunc1* fn) { 
     C.push_back(c); 
     F.push_back(fn);
-    Y0+= c*fn->f(0.0);
   }
 
   inline double f(double x) {
     double res=0;
     for(int i=0; i<C.size(); i++) res += C[i]*F[i]->f(x);
-    return res-Y0;
+    return res;
   }
 
   inline double df(double x) {
@@ -77,8 +98,8 @@ struct ComboFunc1 {
 
     double lap,val,lap0,val0,grad,grad0;
     double dx=0.01;
-    double x0=-0.107*Xmax;
-    double xL=1.307*Xmax;
+    double x0=0.0013;
+    double xL=0.99*Xmax;
     while(x0<xL)
     {
       val=aorb.splint(x0, grad, lap);
@@ -87,14 +108,13 @@ struct ComboFunc1 {
       lap0=d2f(x0);
       fout << x0 << std::setw(20) << val0 << std::setw(20) << val 
         <<std::setw(20) <<  (val-val0)
-        <<std::setw(20) <<  (grad-grad0)/grad0
-        <<std::setw(20) <<  (lap-lap0)/lap0 << std::endl;
-      
-      //val=aorb.splint(x0);
-      //val0=f(x0);
-      //fout << x0 << std::setw(20) << val0 << std::setw(20) << val 
-      //  <<std::setw(20) <<  (val-val0)
-      //  << std::endl;
+        //<<std::setw(20) <<  (grad-grad0)/grad0
+        //<<std::setw(20) <<  (lap-lap0)/lap0 << std::endl;
+        //<<std::setw(20) <<  grad0
+        <<std::setw(20) <<  grad
+        //<<std::setw(20) <<  lap0
+        <<std::setw(20) <<  lap 
+        << std::endl;
       x0+=dx;
     }
   }
@@ -109,7 +129,7 @@ int main(int argc, char** argv) {
   Random.init(0,1,-1);
 
   double ri = 0.0;
-  double rf = 3.145;
+  double rf = 5;
   double xcut=0.23;
   const int nk0=1;
   int npts=101;
@@ -128,38 +148,44 @@ int main(int argc, char** argv) {
   double dL=rf-ri;
   //Create an analytic function for assignment
   ComboFunc1 infunc(dL);
-  infunc.push_back(0.5,new TestFunc1(1,dL));
-  infunc.push_back(-0.3,new TestFunc1(3,dL));
-  infunc.push_back(0.01,new TestFunc1(5,dL));
-  infunc.push_back(-0.01,new TestFunc1(9,dL));
+  infunc.push_back(-0.137958,new TestFunc1(4.55682,rf));
+  infunc.push_back(-1.09405,new TestFunc1(1.83679,rf));
+  infunc.push_back(1.06578,new TestFunc1(4.59201,rf));
+  infunc.push_back(0.96602,new TestFunc1(0.226152,rf));
 
   std::vector<double> inData(npts);
   for(int ix=0; ix<npts; ix++) {
     inData[ix]=infunc.f(gridX(ix));
   }
 
-  OneDimCubicSplinePBC<double,double> aorb(&gridX,inData);
-  aorb.spline();
+  OneDimCubicSpline<double,double> aorb(&gridX,inData);
+  double yp0=(infunc.f(0.001)-infunc.f(0.0))*1000.;
+  aorb.spline(0,yp0,npts-1,0.0);
   infunc.compare(aorb,"cs.dat");
 
   bool closedEnd=true;
-  CubicBspline<double,0,PBC_CONSTRAINTS> borb;
+
+  CubicBspline<double,0,FIRSTDERIV_CONSTRAINTS> borb;
   borb.Init(ri,rf,inData,closedEnd);
   infunc.compare(borb,"bs.dat");
 
-  CubicSpline<double,0,PBC_CONSTRAINTS> corb;
-  corb.Init(ri,rf,inData,closedEnd);
-  infunc.compare(borb,"csnew.dat");
-
   int niter=100000000;
   Timer myTimer;
-  myTimer.restart();
   double sum=0.0,grad,lap;
+  myTimer.restart();
   for(int i=0; i<niter; i++)
   {
     sum += aorb.splint(Random(), grad, lap);
   }
   std::cout << "Cubic spline " << myTimer.elapsed() << " " << sum << std::endl;
+
+  sum=0.0;
+  myTimer.restart();
+  for(int i=0; i<niter; i++)
+  {
+    sum += aorb.splint(Random());
+  }
+  std::cout << "Cubic spline (only value) " << myTimer.elapsed() << " " << sum << std::endl;
 
   sum=0.0;
   myTimer.restart();
@@ -173,9 +199,19 @@ int main(int argc, char** argv) {
   myTimer.restart();
   for(int i=0; i<niter; i++)
   {
+    sum += borb.splint(Random());
+  }
+  std::cout << "Cubic Bspline (only value) " << myTimer.elapsed() << " " << sum << std::endl;
+
+  /*
+  sum=0.0;
+  myTimer.restart();
+  for(int i=0; i<niter; i++)
+  {
     sum += corb.splint(Random(), grad, lap);
   }
   std::cout << "New Cubic Spline " << myTimer.elapsed() << " " << sum << std::endl;
+  */
 
   return 0;
 }
