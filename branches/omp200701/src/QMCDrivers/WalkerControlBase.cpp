00c2765aa6cdf53e1a2fced83199838406d26759
jnkim <jnkim@4bc5593c-822e-11de-b41d-d982dfe7be50>
2007-01-26 22:43:47 +0000

    Remove unused lines.
    git-svn-id: http://qmcpack.googlecode.com/svn@1644 4bc5593c-822e-11de-b41d-d982dfe7be50

--- branches/omp200701/src/QMCDrivers/WalkerControlBase.cpp
+++ branches/omp200701/src/QMCDrivers/WalkerControlBase.cpp
@@ -8,7 +8,6 @@
 //   University of Illinois, Urbana-Champaign
 //   Urbana, IL 61801
 //   e-mail: jnkim@ncsa.uiuc.edu
-//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
 //
 // Supported by 
 //   National Center for Supercomputing Applications, UIUC
@@ -17,7 +16,6 @@
 // -*- C++ -*-
 #include "QMCDrivers/WalkerControlBase.h"
 #include "Particle/HDFWalkerIO.h"
-#include "OhmmsData/OhmmsParameter.h"
 namespace qmcplusplus {
 
   WalkerControlBase::WalkerControlBase(): 
