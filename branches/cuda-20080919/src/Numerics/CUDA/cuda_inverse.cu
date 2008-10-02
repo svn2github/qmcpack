18166a71cd5384a617fdb7b1e84ef7e928f8f52c
kesler <kesler@4bc5593c-822e-11de-b41d-d982dfe7be50>
2008-10-02 19:56:16 +0000

    Adding a kernel for doing multiple matrix inversions at a time.
    git-svn-id: http://qmcpack.googlecode.com/svn@3244 4bc5593c-822e-11de-b41d-d982dfe7be50

--- branches/cuda-20080919/src/Numerics/CUDA/cuda_inverse.cu
+++ branches/cuda-20080919/src/Numerics/CUDA/cuda_inverse.cu
@@ -321,6 +321,107 @@ inverse (T A[], T work[], int N, int stride)
 }
 
 
+
+template<typename T, int BS>
+__global__ void
+inverse_many (T *A_list[], T *work_list[], int N, int stride)
+{
+  int tid = threadIdx.x;
+  __shared__ float *A, *work;
+  if (tid == 0) {
+    A    = A_list[blockIdx.x];
+    work = work_list[blockIdx.x];
+  }
+  __syncthreads();
+
+  float *Atmp = work;
+  float *pivot_tmp = work+N*stride;
+
+  __shared__ T pivot[BS][BS+1], in[BS][BS+1];
+  int NB = N/BS;
+  if (N%BS) NB++;
+
+
+  for (int kb=0; kb<NB; kb++) {
+    // load pivot block
+    int row = kb*BS;
+    for (int j=0; j<BS; j++)
+      if (row+tid < N)
+	pivot[j][tid] = A[(row+j)*stride + row+tid];
+    
+    // invert pivot
+    block_inverse1<T,BS> (pivot);
+
+    // Column scaling
+    int col = kb*BS;
+    for (int jb=0; jb < NB; jb++) {
+      int row = jb*BS;
+      if (kb != jb) {
+    	for (int j=0; j<BS; j++)
+    	  in[j][tid] = -A[(row+j)*stride + col + tid];
+    	block_mul_set<T,BS>(in, pivot, A+row*stride+col, stride);
+      }
+      else {
+    	for (int j=0; j<BS; j++)
+    	  A[(row+j)*stride + col+tid] = 0.0f;
+      }
+    }	
+
+    // Save pivot to global memory here!
+    // We use it for temporary space in the rank-1 update
+    for (int j=0; j<BS; j++)
+      pivot_tmp[j*BS+tid] = pivot[j][tid];
+
+
+    // Copy Ato Atmp
+    for (int ib=0; ib<NB; ib++)
+      for (int row=0; row<N; row++)
+    	Atmp[row*stride+ib*BS+tid] =  A[row*stride+ib*BS+tid];
+    
+    // Rank-1 update
+    for (int ib=0; ib < NB; ib++) {
+      for (int i=0; i<BS; i++)
+    	in[i][tid] = A[(ib*BS+i)*stride + kb*BS + tid];
+      for (int jb=0; jb<NB; jb++) {
+    	for (int i=0; i<BS; i++) {
+    	  pivot[i][tid] = A[(kb*BS+i)*stride + jb*BS + tid];
+    	  // Atmp[(ib*BS+i)*stride + (jb*BS+tid)] = 
+    	  //   A[(ib*BS+i)*stride + (jb*BS+tid)];
+    	}
+    	block_mul_add<T,BS>(in, pivot,  Atmp+(ib*BS)*stride + jb*BS,
+    			    stride);
+      }
+    }
+    // Copy Atmp back to A
+    for (int ib=0; ib<NB; ib++)
+      for (int row=0; row<N; row++)
+    	A[row*stride+ib*BS+tid] =  Atmp[row*stride+ib*BS+tid];
+
+    // Restore pivot from global memory here!
+    for (int j=0; j<BS; j++)
+      pivot[j][tid] = pivot_tmp[j*BS+tid];
+
+    // Row-scaling
+    for (int ib=0; ib<NB; ib++) {
+      int row = kb*BS;
+      int col = ib*BS;
+      if (kb != ib) {
+    	for (int j=0; j<BS; j++)
+    	  in[j][tid] = A[(row+j)*stride + col+tid];
+    	block_mul_set<T,BS>(pivot, in, A+row*stride+col, stride);
+      }
+      else {
+    	for (int j=0; j<BS; j++) 
+    	  A[(row+j)*stride + col+tid] = pivot[j][tid];
+      }
+    }	
+  }
+}
+
+
+
+
+
 #define INVERSE_BS 32
 
 void 
@@ -370,29 +471,55 @@ test_inverse()
 
 
 
-#include <stdio.h>
-
-main()
+void 
+test_inverse_many()
 {
-  test_inverse();
+  int numMats = 1000;
 
-  int N=32;
-  float A[N*N], Acopy[N*N];
-  float *A_d;
+  int N = 128;
+
+  int lwork = N*N + INVERSE_BS * INVERSE_BS;
+  fprintf (stderr, "lwork = %d\n", lwork);
+
+  float **Alist, **worklist;
+  float **Alist_d, **worklist_d;
+
+  Alist    = (float**)malloc(numMats*sizeof(float*));
+  worklist = (float**)malloc(numMats*sizeof(float*));
+  cudaMalloc((void**)&Alist_d,    numMats*sizeof(float*));
+  cudaMalloc((void**)&worklist_d, numMats*sizeof(float*));
+
+  float A[N*N];
+  for (int i=0; i<N*N; i++)
+    A[i] = drand48();
+
+  for (int mat=0; mat<numMats; mat++) {
+    cudaMalloc ((void**)&(Alist[mat]),    N*N*sizeof(float));
+    cudaMalloc ((void**)&(worklist[mat]), lwork*sizeof(float));
+    cudaMemcpy(Alist[mat], A, N*N*sizeof(float), cudaMemcpyHostToDevice);
+  }
+
+  cudaMemcpy(Alist_d   ,    Alist, numMats*sizeof(float*), 
+	     cudaMemcpyHostToDevice);
+  cudaMemcpy(worklist_d, worklist, numMats*sizeof(float*), 
+	     cudaMemcpyHostToDevice);
   
-  for (int i=0; i<N; i++)
-    for (int j=0; j<N; j++)
-      A[N*i+j] = Acopy[N*i+j] = (float) drand48();
+  dim3 dimBlock(32);
+  dim3 dimGrid(numMats);
 
-  cudaMalloc ((void**)&A_d, N*N*sizeof(float));
-  cudaMemcpy (A_d, A, N*N*sizeof(float),
-	      cudaMemcpyHostToDevice);
-
-  dim3 dimBlock(N);
-  dim3 dimGrid(1);
-  block_inverse<float,32><<<dimGrid,dimBlock>>> (A_d, N, N);
-
+  clock_t start = clock();
+  inverse_many<float,INVERSE_BS><<<dimGrid,dimBlock>>> 
+    (Alist_d, worklist_d, N, N);
   cudaThreadSynchronize();
+  clock_t end = clock();
+  
+  double time = (double)(end-start)/(double)CLOCKS_PER_SEC
+    / (double)numMats;
+  double rate = 1.0/time;
+  fprintf (stderr, "Rate is %1.3f matrix inversions per second.\n",
+	   rate);
+
+
   cudaError_t err = cudaGetLastError();
   if (err != cudaSuccess) {
     fprintf (stderr, "CUDA error in block_inverse:\n  %s\n",
@@ -400,17 +527,68 @@ main()
     abort();
   }
 
-  cudaMemcpy (A, A_d, N*N*sizeof(float),
-	      cudaMemcpyDeviceToHost);
+  // Copy Ainv back to host memory
+  float Ainv[N*N];
+  cudaMemcpy(Ainv, Alist[0], N*N*sizeof(float), cudaMemcpyDeviceToHost);
 
-  float nrm = 0.0;
+  float error = 0.0;
   for (int i=0; i<N; i++)
     for (int j=0; j<N; j++) {
       float val = 0.0;
       for (int k=0; k<N; k++)
-	val += A[i*N+k] * Acopy[k*N+j];
-      float diff = (i==j) ? 1.0-val : val;
-      nrm += diff*diff;
+	val += Ainv[i*N+k]*A[k*N+j];
+      float diff = (i==j) ? (1.0f-val) : val;
+      error += diff*diff;
     }
-  fprintf (stderr, "Error = %1.6e\n", sqrt(nrm/(double)(N*N)));
+  fprintf (stderr, "error = %1.8e\n", sqrt(error/(double)(N*N)));
+
+}
+
+
+
+
+
+#include <stdio.h>
+
+main()
+{
+  test_inverse_many();
+
+  // int N=32;
+  // float A[N*N], Acopy[N*N];
+  // float *A_d;
+  
+  // for (int i=0; i<N; i++)
+  //   for (int j=0; j<N; j++)
+  //     A[N*i+j] = Acopy[N*i+j] = (float) drand48();
+
+  // cudaMalloc ((void**)&A_d, N*N*sizeof(float));
+  // cudaMemcpy (A_d, A, N*N*sizeof(float),
+  // 	      cudaMemcpyHostToDevice);
+
+  // dim3 dimBlock(N);
+  // dim3 dimGrid(1);
+  // block_inverse<float,32><<<dimGrid,dimBlock>>> (A_d, N, N);
+
+  // cudaThreadSynchronize();
+  // cudaError_t err = cudaGetLastError();
+  // if (err != cudaSuccess) {
+  //   fprintf (stderr, "CUDA error in block_inverse:\n  %s\n",
+  // 	     cudaGetErrorString(err));
+  //   abort();
+  // }
+
+  // cudaMemcpy (A, A_d, N*N*sizeof(float),
+  // 	      cudaMemcpyDeviceToHost);
+
+  // float nrm = 0.0;
+  // for (int i=0; i<N; i++)
+  //   for (int j=0; j<N; j++) {
+  //     float val = 0.0;
+  //     for (int k=0; k<N; k++)
+  // 	val += A[i*N+k] * Acopy[k*N+j];
+  //     float diff = (i==j) ? 1.0-val : val;
+  //     nrm += diff*diff;
+  //   }
+  // fprintf (stderr, "Error = %1.6e\n", sqrt(nrm/(double)(N*N)));
 }
