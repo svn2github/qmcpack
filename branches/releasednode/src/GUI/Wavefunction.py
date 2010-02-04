affc7a2180adc818a66b82a455a8b836ab48d5bf
jmcminis <jmcminis@4bc5593c-822e-11de-b41d-d982dfe7be50>
2010-02-04 15:57:50 +0000

    removing branch. will add rn to trunk.
    git-svn-id: http://qmcpack.googlecode.com/svn@4552 4bc5593c-822e-11de-b41d-d982dfe7be50

--- branches/releasednode/src/GUI/Wavefunction.py
+++
@@ -1,7 +0,0 @@
-import pygtk
-import gtk
-
-class Wavefunction(gtk.Frame):
-    def __init__(self):
-        gtk.Frame.__init__(self, "Wave function")
-        
--- branches/releasednode/src/dataspork/datasporkpp.cpp
+++
@@ -1,9 +0,0 @@
-#include "DataSporkAnalysis.h"
-
-int main(int ac, char* av[]) {
-
-  DataSporkAnalysis ds;
-  int dummy = ds.getOptions(ac,av);
-  ds.execute();
-  return dummy;
-}
