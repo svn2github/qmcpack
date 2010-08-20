57f3ffbdd201a8fc698d1f0896c7c7e63270fb12
jeongnim.kim <jeongnim.kim@4bc5593c-822e-11de-b41d-d982dfe7be50>
2010-08-20 18:31:49 +0000

    fine-grained evaluation.
    git-svn-id: http://qmcpack.googlecode.com/svn@4945 4bc5593c-822e-11de-b41d-d982dfe7be50

--- branches/mixed-r4938/src/SandBox/mixed.cpp
+++ branches/mixed-r4938/src/SandBox/mixed.cpp
@@ -5,11 +5,82 @@
 #include <Utilities/RandomGenerator.h>
 #include <Utilities/Timer.h>
 #include <Utilities/IteratorUtility.h>
+
+namespace qmcplusplus {
+  template<typename EngT>
+    class einspline_omp
+    {
+      typedef einspline_engine<EngT> spliner_type;
+      enum {DIM=spliner_type::DIM};
+      vector<spliner_type*> engines;
+
+      public:
+      einspline_omp(){}
+
+      ~einspline_omp()
+      {
+        delete_iter(engines.begin(),engines.end());
+      }
+
+      void create_plan(const TinyVector<int,DIM>& ng, int nsplines, bool randomize=false)
+      {
+        if(engines.empty())
+          engines.resize(omp_get_max_threads(),0);
+#pragma omp parallel
+        {
+          int ip=omp_get_thread_num();
+          int ns_loc=nsplines/engines.size();
+          if(engines[ip]) delete engines[ip];
+          engines[ip]=new spliner_type(ng,ns_loc);
+          if(randomize)
+          {
+            Array<typename spliner_type::value_type, 3> data(ng[0], ng[1], ng[2]);
+            for (int i = 0; i < ns_loc; ++i)
+            {
+              for (int j = 0; j < data.size(); ++j)
+                data(j) = Random();
+              engines[ip]->set(i, data.data(), data.size());
+            }
+          }
+        }
+      }
+
+      template<typename T1>
+      void evaluate_v(const TinyVector<T1,DIM>& here)
+      {
+        const int np=engines.size();
+#pragma omp parallel for
+        for(int ip=0; ip<np; ++ip) engines[ip]->evaluate_v(here);
+      }
+
+      template<typename T1>
+      void evaluate_v(const vector<TinyVector<T1,DIM> >& here)
+      {
+        const int np=engines.size();
+#pragma omp parallel for
+        for(int ip=0; ip<np; ++ip) 
+        {
+          for(int i=0; i<here.size(); ++i)
+            engines[ip]->evaluate_v(here[i]);
+        }
+      }
+
+      template<typename T1>
+      void evaluate_vgh(const TinyVector<T1,DIM>& here)
+      {
+        const int np=engines.size();
+#pragma omp parallel for
+        for(int ip=0; ip<np; ++ip) engines[ip]->evaluate_vgh(here);
+      }
+    };
+}
+
 using namespace qmcplusplus;
 
 int main(int argc, char **argv)
 {
 
+  Random.init(0,1,11);
   //einspline_test<multi_UBspline_3d_s> sp_s;
   //einspline_test<multi_UBspline_3d_d> sp_d;
   //einspline_test<multi_UBspline_3d_c> sp_c;
@@ -20,6 +91,7 @@ int main(int argc, char **argv)
 
   TinyVector<int,3> ng(48);
   int num_splines=128;
+  int num_samples=14;
   int niters=1000;
   int opt;
 
@@ -43,9 +115,9 @@ int main(int argc, char **argv)
       case 's':
         num_splines=atoi(optarg);
         break;
-//      case 'p':
-//        nsamples=atoi(optarg);
-//        break;
+      case 'p':
+        num_samples=atoi(optarg);
+        break;
       case 'i':
         niters=atoi(optarg);
         break;
@@ -53,37 +125,32 @@ int main(int argc, char **argv)
   }
 
   typedef einspline_engine<multi_UBspline_3d_d> spliner_type;
-
-  spliner_type master_engine(ng, num_splines);
+  einspline_omp<multi_UBspline_3d_d> myeng;
+  myeng.create_plan(ng,num_splines,true);
+  RandomGenerator_t rng;
+  Timer clock;
+  for (int i = 0; i < niters; ++i)
   {
-    Array<value_type, 3> data(ng[0], ng[1], ng[2]);
-    for (int i = 0; i < num_splines; ++i)
-    {
-      for (int j = 0; j < data.size(); ++j)
-        data(j) = Random();
-      master_engine.set(i, data.data(), data.size());
-    }
+    vector<TinyVector<real_type, 3> > here(num_samples);
+    for(int k=0; k<num_samples; ++k)
+      here[k]=TinyVector<real_type,3>(rng(), rng(), rng());
+    myeng.evaluate_v(here);
   }
-
-
+  double dt_v=clock.elapsed();
+  for (int i = 0; i < niters; ++i)
   {
-    vector<spliner_type*> myeng(omp_get_max_threads());
-    Timer clock;
-#pragma parallel omp
-    {
-      int ip = omp_get_thread_num();
-      myeng[ip] = new spliner_type(master_engine);
-      RandomGenerator_t rng;
-      for (int i = 0; i < niters; ++i)
-      {
-        TinyVector<real_type, 3> here(rng(), rng(), rng());
-        myeng[ip]->evaluate_v(here);
-      }
-    }
-    double dt=clock.elapsed();
-    cout << "Timing = " << dt << " nops=" << niters*num_splines/dt << endl;
-    delete_iter(myeng.begin(), myeng.end());
+    TinyVector<real_type, 3> here(rng(), rng(), rng());
+    myeng.evaluate_vgh(here);
   }
+  double dt_vgh=clock.elapsed();
+  cout.setf(std::ios::scientific, std::ios::floatfield);
+  cout.precision(6);
+  cout << "einspline_omp " << setw(3) << omp_get_max_threads() 
+    <<setw(6) << ng[0] << setw(6) << num_splines << setw(6) << num_samples
+    << "  " << niters*num_samples*num_splines/dt_v
+    << "  " << niters*num_splines/dt_vgh
+    << endl;
+
 //  einspline_engine<multi_UBspline_3d_s> spliner(ng,num_splines);
 //  spliner.evaluate_v(here);
 //  cout << spliner.Val << endl;
